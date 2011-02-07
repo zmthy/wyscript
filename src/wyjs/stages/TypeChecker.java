e90417a02c24f23cab2f50b7da13d49ea39e77f6
DavePearce <david.pearce@ecs.vuw.ac.nz>
2011-02-07 15:10:07 +1300

    Minor bug fix for subtyping of functions.

--- src/wyjs/stages/TypeChecker.java
+++ src/wyjs/stages/TypeChecker.java
@@ -723,16 +723,9 @@ public class TypeChecker {
           || (receiver != null && funrec != null && Type.isSubtype(funrec,
               receiver))) {
         // receivers match up OK ...
-        if (ft.params.size() == paramTypes.size() && Type.isSubtype(ft, target)
-            && (candidate == null || Type.isSubtype(candidate, ft))) {
-          // This declaration is a candidate. Now, we need to see if
-          // our
-          // candidate type signature is as precise as possible.
-          if (candidate == null) {
-            candidate = ft;
-          } else if (Type.isSubtype(candidate, ft)) {
-            candidate = ft;
-          }
+        if (ft.params.size() == paramTypes.size() && paramSubtypes(ft, target)
+            && (candidate == null || paramSubtypes(candidate, ft))) {
+          candidate = ft;         
         }
       }
     }
@@ -759,6 +752,20 @@ public class TypeChecker {
 
     return candidate;
   }
+  
+  private boolean paramSubtypes(Type.Fun f1, Type.Fun f2) {
+		List<Type> f1_params = f1.params;
+		List<Type> f2_params = f2.params;
+		if(f1_params.size() == f2_params.size()) {
+			for(int i=0;i!=f1_params.size();++i) {
+				if(!Type.isSubtype(f1_params.get(i),f2_params.get(i))) {
+					return false;
+				}
+			}
+			return true;
+		}
+		return false;
+	}
 
   private String parameterString(List<Type> paramTypes) {
     String paramStr = "(";
